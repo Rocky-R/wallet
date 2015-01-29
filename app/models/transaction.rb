beef029de47bd989b9ba50e19264d89e75ffff1a
Rocky Randalls <rrandalls@gmail.com>
2015-01-29 11:57:53 -0500

    Add validation for numericals and empty fields

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,13 @@
 class Transaction < ActiveRecord::Base
+
+  validates :transaction_description, presence: true
+  validates :credit, presence: true,
+            unless: -> (transaction) { transaction.debit.present? }
+  validates :debit, presence: true,
+            unless: -> (transaction) { transaction.credit.present? }
+  validates :credit, numericality: true, allow_nil: true
+  validates :debit, numericality: true, allow_nil: true
+
   def self.balance
     sum(:debit) + sum(:credit)
   end
